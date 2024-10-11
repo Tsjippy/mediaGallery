048a48b4ba9823840645cd13da53b272393cb00e
Tsjippy <ewaldha@hotmail.com>
2024-10-11 08:34:13 +0100

    added: MODULE_PATH contstant

--- php/__module_menu.php
+++ php/__module_menu.php
@@ -5,6 +5,9 @@ use SIM;
 use function SIM\printArray;
 
 const MODULE_VERSION		= '8.0.1';
+
+DEFINE(__NAMESPACE__.'\MODULE_PATH', plugin_dir_path(__DIR__));
+
 //module slug is the same as grandparent folder name
 DEFINE(__NAMESPACE__.'\MODULE_SLUG', strtolower(basename(dirname(__DIR__))));
 
